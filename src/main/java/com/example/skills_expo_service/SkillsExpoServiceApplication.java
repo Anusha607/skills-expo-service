ea52f01ccb42ff4dc33c1b8978efe5da29f08667
Anusha.T <anusha.t@apcfss.in>
2025-09-17 12:17:27 +0530

    initial commit

---
+++ src/main/java/com/example/skills_expo_service/SkillsExpoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.skills_expo_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsExpoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsExpoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/skills_expo_service/SkillsExpoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skills_expo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsExpoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
